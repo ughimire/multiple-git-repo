116bec625c04e0966051cd28180ca17ddd7e4c5d
Umesh Ghimire <me.umeshuser@gmail.com>
2022-12-27 14:49:22 +0545

    init

---
+++ multiple-clone.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+# You can use this command into your terminal: bash multiple-clone.sh *.txt
+readarray array <<<$(cat "$@")
+
+#mkdir -p ~/git && cd ~/git
+cd ../wp-content/plugins/ #// Your Parent Folder Directory
+for element in ${array[@]}; do
+  base_name=$(basename $element)
+  folder_name=${base_name/.git/""}
+
+  #rm -rf $folder_name // You can enable only if you want to remove existing folder ( may be git folder )
+  echo "clonning $element into $PWD/$folder_name"
+  git clone $element
+done
